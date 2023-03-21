2ed71630c84a9091007b2a775ac4c974ee0b2733
ShafiaChy <shafiachy04@gmail.com>
2023-03-21 17:54:30 +0600

    let's see

---
+++ src/pages/Home/test1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const test1 = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde fugiat nam, possimus aut pariatur enim blanditiis vel eveniet excepturi nemo deleniti, ad, laborum ipsam nulla commodi adipisci dicta rerum sit?</p>
+        </div>
+    );
+};
+
+export default test1;
---
+++ src/pages/Home/test3.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const test3 = () => {
+    return (
+        <div>
+            <a href="http://"></a>
+        </div>
+    );
+};
+
+export default test3;
