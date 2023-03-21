097683ce31d85aac7739219fb508ebff9a10e406
ShafiaChy <shafiachy04@gmail.com>
2023-03-21 18:16:57 +0600

    try

---
+++ src/pages/Home/sample23.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const sample23 = () => {
+    return (
+        <div>
+            sjjjsjjs
+        </div>
+    );
+};
+
+export default sample23;
---
+++ src/pages/Home/sample24.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const sample24 = () => {
+    return (
+        <div>
+            djjdjdjjdjdjdjdj
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, quisquam eligendi corrupti ipsa doloremque fuga reprehenderit accusamus voluptatibus fugit tempora enim sed mollitia dolorem quaerat asperiores ut eum dignissimos neque?
+        </div>
+    );
+};
+
+export default sample24;
