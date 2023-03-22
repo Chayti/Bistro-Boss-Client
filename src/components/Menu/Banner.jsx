59b35ab310370443018ef283550fbaf1188fead7
Chayti Saha <chaytisaha98@gmail.com>
2023-03-22 20:50:29 +0600

    menu page done

--- src/components/Menu/Banner.jsx
+++ src/components/Menu/Banner.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Banner.css';
+import './Menu.css';
 
 export function Banner() {
 
+
     return (
         <div className="container-lg">
-            <div className="bg-img flex justify-center items-center">
+            <div className="top-banner bg-img flex justify-center items-center">
                 <div className='section text-style text-white flex justify-center items-center font-bold'>
                     <div className="text-center">
                         <h1 className='uppercase text-7xl '>Our Menu</h1>
---
+++ src/components/Menu/Dessert.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card } from '../shared/Card';
+
+export const Dessert = ({ items }) => {
+    console.log(items)
+
+    return (
+        <>
+            <div className="container-lg my-20">
+                <div className="dessert-banner bg-img2 flex justify-center items-center">
+                    <div className='section2 text-style text-white flex justify-center items-center font-bold'>
+                        <div className="text-center">
+                            <h1 className='uppercase text-7xl '>Desserts</h1>
+                            <br />
+                            <small className='text-lg'>Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-5 px-10">
+                {
+                    items?.map((item) =>
+                        <div className="flex space-x-4">
+                            <Card>
+                                {item}
+                            </Card>
+                        </div>
+                    )
+                }
+            </div>
+        </>
+
+    );
+};
