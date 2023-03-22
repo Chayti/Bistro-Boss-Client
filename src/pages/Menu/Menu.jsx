f83f412b59c024a01e366417e48cd0838cd4f4dd
Chayti Saha <chaytisaha98@gmail.com>
2023-03-22 18:38:06 +0600

    created json for menu page

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Banner } from '../../components/Menu/Banner/Banner';
+import './Menu.css'
 
 const Menu = () => {
     return (
         <div>
             <Banner></Banner>
+            <div class="menu-bg">
+                hello
+                geloo
+                hwloo
+            </div>
         </div>
     );
 };
