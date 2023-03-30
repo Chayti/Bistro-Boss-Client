51da9d9405bd27ad98a1430d0d0fa27d97319f2f
Chayti Saha <chaytisaha98@gmail.com>
2023-03-30 15:43:31 +0600

    book table btn style updated

--- src/pages/Dashboard/ReservationForm.jsx
+++ src/pages/Dashboard/ReservationForm.jsx
@@ -1,13 +1,12 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { FaTable } from "react-icons/fa";
 import InputField from '../../components/shared/InputField';
-import Title from '../../components/shared/Title/Title';
 
 
 const ReservationForm = () => {
 
-    const options=[
+    const options = [
         '1 person',
         '2 person',
         '3 person',
@@ -28,39 +27,39 @@ const ReservationForm = () => {
     console.log(selectedTime)
     return (
         <div className=''>
-          
+
             <form className="rounded-lg" >
                 <div className="flex flex-wrap mb-4">
-                <div className="w-full md:w-1/3 px-4 mb-4 md:mb-0 ">
-                       
-                <InputField
+                    <div className="w-full md:w-1/3 px-4 mb-4 md:mb-0 ">
+
+                        <InputField
                             label="date"
                             name="date"
                             type="date"
                             onChange={(e) => setSelectedDate(e.target.value)}
                         />
-                       
+
                     </div>
                     <div className="w-full md:w-1/3 px-4 mb-4 md:mb-0 ">
-                       
-                   <InputField
+
+                        <InputField
                             label="time"
                             name="time"
                             type="time"
                             onChange={(e) => setSelectedTime(e.target.value)}
                         />
-                       
+
                     </div>
                     <div className="w-full md:w-1/3 px-4 mb-4 md:mb-0 ">
-                    <label htmlFor='guests'>Guests</label>
-                    <select name='guests' id='guests' className="form-control border-2 border-gray-200 block w-full rounded-md py-2 px-3 text-gray-700 placeholder-gray-400">
-                        <option value='' />
-                        {options.map((option, index) => (
-                            (index === 0)
-                            ? <option selected key={index} value={index}>{option}</option>
-                            : <option key={index} value={index}>{option}</option>
-                        ))}
-                    </select>
+                        <label htmlFor='guests'>Guests</label>
+                        <select name='guests' id='guests' className="form-control border-2 border-gray-200 block w-full rounded-md py-2 px-3 text-gray-700 placeholder-gray-400">
+                            <option value='' />
+                            {options.map((option, index) => (
+                                (index === 0)
+                                    ? <option selected key={index} value={index}>{option}</option>
+                                    : <option key={index} value={index}>{option}</option>
+                            ))}
+                        </select>
                     </div>
 
                     <div className="w-full md:w-1/3 px-4 mb-4 md:mb-0 ">
@@ -70,20 +69,20 @@ const ReservationForm = () => {
                             type="text"
                             placeholder="Your Name"
                             value={name}
-                            
+
                             onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div className="w-full md:w-1/3 px-4">
-                    <InputField
-                        label="Phone"
-                        name="phone"
-                        type="text"
-                        placeholder="Phone number"
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                    />
-                   </div>
+                        <InputField
+                            label="Phone"
+                            name="phone"
+                            type="text"
+                            placeholder="Phone number"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </div>
                     <div className="w-full md:w-1/3 px-4">
                         <InputField
                             label="Email"
@@ -94,14 +93,16 @@ const ReservationForm = () => {
                             onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
-                   
+
                 </div>
-              
-                <div className="text-center mt-8">
-                    <button onClick={(e)=>
-                        {e.preventDefault() 
-                        navigate('/dashboard/payment')}} className={"btn4 py-2 px-8 "}>
+
+                <div className="text-center mt-8 ">
+                    <button onClick={(e) => {
+                        e.preventDefault()
+                        navigate('/dashboard/payment')
+                    }} className={"btn4 py-2 px-8 inline-flex items-center"}>
                         Book A Table
+                        <FaTable className="ml-2" style={{ verticalAlign: 'middle' }} />
                     </button>
                 </div>
             </form >
