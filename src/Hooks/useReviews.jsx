d122f0237a92d418b7bb18911d4805f0cbf59897
ShafiaChy <shafiachy04@gmail.com>
2023-04-28 19:03:13 +0600

    cart, user profile updated

---
+++ src/Hooks/useReviews.jsx
@@ -0,0 +1,24 @@
+// import { useQuery } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
+import useAuth from './useAuth';
+
+const useReviews = () => {
+    const { user } = useAuth()
+    const [loading, setLoading] = useState(true);
+    const [reviews, setReviews] = useState([]);
+   console.log(reviews)
+    useEffect(() => {
+        fetch(`http://localhost:5000/review?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+                setLoading(false)
+                
+            }
+            )
+    }, [])
+
+    return [reviews, loading, setReviews];
+};
+
+export default useReviews;
