f7ef3944506c959f32d58676c3b67adb479309b6
Chayti Saha <chaytisaha98@gmail.com>
2023-05-09 18:22:22 +0600

    some assets added, home page slider updated

--- src/components/shared/Banner/Banner.jsx
+++ src/components/shared/Banner/Banner.jsx
@@ -11,39 +11,12 @@ import SliderFive from '../../../assets/asset/home/05.jpg'
 const Banner = ({ banner }) => {
     return (
         <Carousel autoPlay >
-                <div>
-                    <img src={SliderOne} />
-                   
-                </div>
-                <div>
-                    <img src={SliderTwo} />
-                  
-                </div>
-                <div>
-                    <img src={SliderThree}/>
-                    
-                    
-                </div>
-                <div>
-                    <img src={SliderFour}/>
-                    
-                    
-                </div>
-                <div>
-                    <img src={SliderFive}/>
-                    
-                    
-                </div>
-            </Carousel>
-        //     <div className="text-center text-white flex relative top-0 justify-center items-center h-[100vh] bg-home-image">
-        //     <div className='text-style'>
-            
-        //         <h1 className=' uppercase text-7xl '>{banner.title}</h1>
-        //         <small className='text-lg'>{banner.description}</small>
-        //     </div>
-        // </div>
-
-
+            <div><img src={SliderTwo} /></div>
+            <div><img src={SliderThree} /></div>
+            <div><img src={SliderOne} /></div>
+            <div><img src={SliderFour} /></div>
+            <div><img src={SliderFive} /></div>
+        </Carousel>
     );
 };
 
